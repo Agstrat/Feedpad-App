7149686449081bf7a30e56f47110e0f62697570d
Agstrat <peter@agstrat.co>
2025-11-01 19:12:15 +1100

    Update Start.tsx

--- src/pages/Start.tsx
+++ src/pages/Start.tsx
@@ -1,14 +1,13 @@
-// src/pages/Start.tsx
 import { Link } from 'react-router-dom';
 
 export default function Start() {
   return (
     <div className="card out">
       <h1 className="v">FeedPad</h1>
-      <p>Set defaults once. Calculate anywhere. Works offline when installed.</p>
+      <p>Set defaults once. Calculate anywhere.</p>
 
       <div style={{ display: 'flex', gap: 12, marginTop: 18 }}>
-        <Link to="/defaults" className="btn">Default Settings</Link>
+        <Link to="/defaults" className="btn">Update Defaults</Link>
         <Link to="/calculator" className="btn">Start Calculations</Link>
       </div>
     </div>
