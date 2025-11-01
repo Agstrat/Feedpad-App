1eae7b45602cf658c920ec23a9330093f955f8e8
Agstrat <peter@agstrat.co>
2025-11-01 19:45:59 +1100

    Update Start.tsx

--- src/pages/Start.tsx
+++ src/pages/Start.tsx
@@ -4,8 +4,7 @@ export default function Start() {
   return (
     <div className="card out">
       <h1 className="v">FeedPad</h1>
-      <p>Set defaults once. Calculate anywhere.</p>
-
+      <p>Set defaults once. Calculate anywhere. Works offline when installed.</p>
       <div style={{ display: 'flex', gap: 12, marginTop: 18 }}>
         <Link to="/defaults" className="btn">Update Defaults</Link>
         <Link to="/calculator" className="btn">Start Calculations</Link>
